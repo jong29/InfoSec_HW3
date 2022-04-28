b08dd6234f89d1e2de32006ac324ab8521573fe6
jong29 <jongwoop97@gmail.com>
2022-04-29 01:02:08 +0900

    added cryp.c with specs

---
+++ cryp.c
@@ -0,0 +1,5 @@
+// support two modes enc, dec
+// read input file and encrypt/decrypt file content
+// needs to perform authenticated encryption using HMAC-SHA256
+// --> this should produce a HMAC-SHA256 tag(message authentication code)
+// also needs a shared.key file with the key
